5c5f560ae91735667bdd363aeae75f827e20efee
raejohnsen <johnr486@newschool.edu>
2023-11-28 00:57:32 -0500

    edits (JS still not working)

---
+++ entries/entry11/entry/main.js
@@ -0,0 +1,12 @@
+// function paused() {
+//     console.log("Scroll paused");
+//       }
+      
+//       let wrapper = document.querySelector(".wrapper");
+//       wrapper.addEventListener("click", paused());
+    
+
+    
+//     function paused() {
+//       wrapper.classList.toggle("pause");
+//     }
