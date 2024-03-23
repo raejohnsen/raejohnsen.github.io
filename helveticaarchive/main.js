016bf4efa83b3e7df9d3b06a1131a4e8c553749e
raejohnsen <johnr486@newschool.edu>
2024-03-22 21:34:04 -0400

    helvetica stuff

--- helveticaarchive/main.js
+++ helveticaarchive/main.js
@@ -15,37 +15,102 @@ window.addEventListener('scroll', function() {
     }
 });
 
-function getPics() {} //just for this demo
-const imgs = document.querySelectorAll('.gallery img');
-const fullPage = document.querySelector('#fullpage');
-
-imgs.forEach(img => {
-  img.addEventListener('click', function() {
-    fullPage.style.backgroundImage = 'url(' + img.src + ')';
-    fullPage.style.display = 'block';
-    image.style.display = 'fixed';
-  });
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-    const targetDiv = document.getElementById('targetDiv');
-    const images = document.querySelectorAll('.image');
-  
-    images.forEach(image => {
-      image.addEventListener('click', function() {
-        toggleVisibility(targetDiv);
-      });
-    });
-
-    fullPage.addEventListener('click', function() {
-        targetDiv.style.display = 'flex';
-    });
-  
-    function toggleVisibility(element) {
-      if (element.style.display === 'none') {
-        element.style.display = 'block';
-      } else {
-        element.style.display = 'none';
-      }
+function toggleClass(elementId) {
+    var element = document.getElementById(elementId);
+    if (element.classList.contains('toggleOn')) {
+        element.classList.remove('toggleOn');
+    } else {
+        element.classList.add('toggleOn');
     }
-  });
+}
+
+// function toggleClass() {
+//   var element = document.getElementById("green");
+//   element.classList.toggle("toggleOn");
+// }
+
+// function toggleClass() {
+//   var element = document.getElementById("construct");
+//   element.classList.toggle("toggleOn");
+// }
+// function toggleClass() {
+//   var element = document.getElementById("jacobs");
+//   element.classList.toggle("toggleOn");
+// }
+// function toggleClass() {
+//   var element = document.getElementById("paper");
+//   element.classList.toggle("toggleOn");
+// }
+// function toggleClass() {
+//   var element = document.getElementById("nasa");
+//   element.classList.toggle("toggleOn");
+// }
+// function toggleClass() {
+//   var element = document.getElementById("posters");
+//   element.classList.toggle("toggleOn");
+// }
+// function toggleClass() {
+//   var element = document.getElementById("belafonte");
+//   element.classList.toggle("toggleOn");
+// }
+
+// let greenImg = document.getElementById("green");
+// greenImg.addEventListener("click", function() {;
+//     greenImg.style.width = "100vw";
+//     greenImg.style.height = "auto";
+//     greenImg.style.zIndex = "9998";
+//     greenImg.style.position = "absolute";
+// });
+
+// let constructImg = document.getElementById("construct");
+// constructImg.addEventListener("click", function() {;
+//     constructImg.style.width = "100vw";
+//     constructImg.style.height = "auto";
+//     constructImg.style.top = "110px";
+//     constructImg.style.zIndex = "9998";
+//     constructImg.style.position = "absolute";
+// });
+
+// let jacobsImg = document.getElementById("jacobs");
+// jacobsImg.addEventListener("click", function() {;
+//     jacobsImg.style.width = "100vw";
+//     jacobsImg.style.height = "auto";
+//     jacobsImg.style.top = "110px";
+//     jacobsImg.style.zIndex = "9998";
+//     jacobsImg.style.position = "absolute";
+// });
+
+// function getPics() {} //just for this demo
+// const imgs = document.querySelectorAll('.gallery img');
+// const fullPage = document.querySelector('#fullpage');
+
+// imgs.forEach(img => {
+//   img.addEventListener('click', function() {
+//     fullPage.style.backgroundImage = 'url(' + img.src + ')';
+//     fullPage.style.display = 'block';
+//     image.style.display = 'fixed';
+//   });
+// });
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     const targetDiv = document.getElementById('targetDiv');
+//     const images = document.querySelectorAll('.image');
+  
+//     images.forEach(image => {
+//       image.addEventListener('click', function() {
+//         toggleVisibility(targetDiv);
+//       });
+//     });
+
+//     fullPage.addEventListener('click', function() {
+//         targetDiv.style.display = 'flex';
+//     });
+  
+//     function toggleVisibility(element) {
+//       if (element.style.display === 'none') {
+//         element.style.display = 'block';
+//       } else {
+//         element.style.display = 'none';
+//       }
+//     }
+//   });
---
+++ helveticaarchive/test.js
@@ -0,0 +1,9 @@
+let background = document.getElementById("targetDiv");
+
+let jacobsImg = document.getElementById("jacobs");
+jacobsImg.addEventListener("click", function() {;
+    jacobsImg.style.width = "100vw";
+    jacobsImg.style.height = "auto";
+    background.style.display = "none";
+});
+
