3c12edf01c8c143a243c4ab44a11bc5f506de5cc
raejohnsen <johnr486@newschool.edu>
2024-10-09 13:19:02 -0400

    why isn't this working

---
+++ cornerstones/love/main.js
@@ -0,0 +1,58 @@
+let sam = document.getElementById("sam");
+let ahmed = document.getElementById("ahmed");
+let d396 = document.getElementById("d396");
+let bueno = document.getElementById("bueno");
+let aguila = document.getElementById("aguila");
+let manayeyo = document.getElementById("manayeyo");
+
+
+
+sam.onmouseover = function(){
+    sam.src = "url('sources/samcolor.jpg')";
+}
+
+ahmed.onmouseover = function(){
+    ahmed.src = "url('sources/ahmedcolor.jpg')";
+}
+
+d396.onmouseover = function(){
+    d396.src = "url('sources/d396color.jpg')";
+}
+
+bueno.onmouseover = function(){
+    bueno.src = "url('sources/buenocolor.jpg')";
+}
+
+aguila.onmouseover = function(){
+    aguila.src = "url('sources/aguilacolor.jpg')";
+}
+
+manayeyo.onmouseover = function(){
+    manayeyo.src = "url('sources/manayeyocolor.jpg')";
+}
+
+
+
+sam.onmouseout = function(){
+    sam.src = "url('sources/sam.png')";
+}
+
+ahmed.onmouseout = function(){
+    ahmed.src = "url('sources/ahmed.png')";
+}
+
+d396.onmouseout = function(){
+    d396.src = "url('sources/396.png')";
+}
+
+bueno.onmouseout = function(){
+    bueno.src = "url('sources/bueno.png')";
+}
+
+aguila.onmouseout = function(){
+    aguila.src = "url('sources/aguila.png')";
+}
+
+manayeyo.onmouseout = function(){
+    manayeyo.src = "url('sources/manayeyo.png')";
+}
